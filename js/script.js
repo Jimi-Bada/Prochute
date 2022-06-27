dde5ca048451b5dc118d16e738067634d84a6b83
Jimi-Bada <jimibadaadedamola@gmail.com>
2022-06-27 14:46:46 +0100

    new

---
+++ js/script.js
@@ -0,0 +1,14 @@
+;
+// console.log(select);
+
+function resize() {
+    let differentCategories = document.querySelectorAll('#Categories');
+    // differentCategories={};
+    // console.log(differentCategories);
+    console.log( differentCategories.every((element, index) => { /* ... */ } ));
+   
+    let select = document.getElementById('options').innerHTML;
+    select.style.width =differentCategories.length*100+'px'; 
+    console.log(select);
+}
+resize();
