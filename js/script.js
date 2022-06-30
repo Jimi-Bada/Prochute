13f1ad66fbda325a6c25f5180699a42e67e56e9c
Jimi-Bada <jimibadaadedamola@gmail.com>
2022-06-30 21:47:46 +0100

    new

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 ;
 // console.log(select);
 
-function resize() {
-    let differentCategories = document.querySelectorAll('#Categories');
-    // differentCategories={};
-    // console.log(differentCategories);
-    console.log( differentCategories.every((element, index) => { /* ... */ } ));
+// function resize() {
+//     let differentCategories = document.querySelectorAll('#Categories');
+//     // differentCategories={};
+//     // console.log(differentCategories);
+//     console.log( differentCategories.every((element, index) => { /* ... */ } ));
    
-    let select = document.getElementById('options').innerHTML;
-    select.style.width =differentCategories.length*100+'px'; 
-    console.log(select);
-}
-resize();
+//     let select = document.getElementById('options').innerHTML;
+//     select.style.width =differentCategories.length*100+'px'; 
+//     console.log(select);
+// }
+// resize();
+
+
+const modal  = document.querySelector('.modal');
+const closeModalIcon = document.querySelector(".bi-x-lg");
+
+
+
+closeModalIcon.addEventListener('click',()=>{
+    modal.closeModal();
+
+})
